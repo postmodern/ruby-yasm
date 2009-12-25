949a4eed989c8b5a785371904553b594db6886ad
postmodern <postmodern.mod3@gmail.com>
2009-12-24 20:03:00 -0800

    Added custom names to the YASM::Task options.

--- lib/yasm/task.rb
+++ lib/yasm/task.rb
@@ -8,13 +8,29 @@ module YASM
     long_option :flag => '--help'
     long_option :flag => '--arch', :equals => true
     long_option :flag => '--parser', :equals => true
-    long_option :flag => '--preproc', :equals => true
-    long_option :flag => '--oformat', :equals => true
-    long_option :flag => '--dformat', :equals => true
-    long_option :flag => '--lformat', :equals => true
-    long_option :flag => '--list', :equals => true
-    long_option :flag => '--objfile', :equals => true
-    long_option :flag => '--mapfile', :equals => true
+    long_option :flag => '--preproc',
+                :equals => true,
+                :name => :preprocessor
+    long_option :flag => '--oformat',
+                :equals => true,
+                :name => :output_format
+    long_option :flag => '--dformat',
+                :equals => true,
+                :name => :debugging_format
+    long_option :flag => '--lformat',
+                :equals => true,
+                :name => :list_format
+
+    long_option :flag => '--list',
+                :equals => true,
+                :name => :list_file
+    long_option :flag => '--objfile',
+                :equals => true,
+                :name => :output
+    long_option :flag => '--mapfile',
+                :equals => true,
+                :name => :map_file
+
     long_option :flag => '--machine', :equals => true
     long_option :flag => '--force-strict'
     short_option :flag => '-w', :name => :inhibit_warnings
@@ -22,7 +38,7 @@ module YASM
     short_option :flag => '-M', :name => :gen_makefile_deps
     short_option :flag => '-E', :name => :redirect_errors_to
     short_option :flag => '-s', :name => :redirect_errors
-    long_option :flag => '--preproc-only'
+    long_option :flag => '--preproc-only', :name => :preprocessor_only
     short_option :flag => '-I', :name => :include, :multiple => true
     short_option :flag => '-P', :name => :pre_include, :multiple => true
     short_option :flag => '-D', :name => :define, :multiple => true
