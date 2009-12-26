59f0b04f24bafeb3b4df53c2aeb41a1f2479e892
postmodern <postmodern.mod3@gmail.com>
2009-12-25 19:24:30 -0800

    Added YARD tags to YASM::Program.

--- lib/yasm/program.rb
+++ lib/yasm/program.rb
@@ -8,14 +8,62 @@ module YASM
 
     name_program 'yasm'
 
+    #
+    # Finds the +yasm+ program and assembles a file.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Additional options for yasm.
+    #
+    # @yield [task]
+    #   If a block is given, it will be passed a task object used to
+    #   specify options for yasm.
+    #
+    # @yieldparam [Task]
+    #   The yasm task object.
+    #
+    # @return [Boolean]
+    #   Specifies whether the command exited normally.
+    #
     def self.assmeble(options={},&block)
       self.find.assemble(options,&block)
     end
 
+    #
+    # Assembles an assembly file.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Additional options for yasm.
+    #
+    # @yield [task]
+    #   If a block is given, it will be passed a task object used to
+    #   specify options for yasm.
+    #
+    # @yieldparam [Task]
+    #   The yasm task object.
+    #
+    # @return [Boolean]
+    #   Specifies whether the command exited normally.
+    #
     def assemble(options={},&block)
       run_task(Task.new(options,&block)
     end
 
+    #
+    # Assembles an assembly file and writes the output to a temporary file.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Additional options for yasm.
+    #
+    # @yield [task]
+    #   If a block is given, it will be passed a task object used to
+    #   specify options for yasm.
+    #
+    # @yieldparam [Task]
+    #   The yasm task object.
+    #
+    # @return [TempFile]
+    #   The temporary file containing the assembled object code.
+    #
     def assemble_temp(options={},&block)
       task = Task.new(options,&block)
       task.output = TempFile.new('yasm')
